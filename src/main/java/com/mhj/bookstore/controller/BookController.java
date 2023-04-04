73a41cd2d8f3343f467d3f2b06b026e994f36c82
JABRANE MOHAMED <mohamed-hedi.jabrane@externe.grdf>
2023-04-04 09:12:07 +0200

    add travis file

--- src/main/java/com/mhj/bookstore/controller/BookController.java
+++ src/main/java/com/mhj/bookstore/controller/BookController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * @author GRDF
  */
 @RestController
 public class BookController {
--- src/main/java/com/mhj/bookstore/controller/BookDto.java
+++ src/main/java/com/mhj/bookstore/controller/BookDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @author GRDF
  */
 
 @Data
